000a9f427812bcf4b1ee6b29202ddf3f9831b746
Alyssa <125580811+ajpupping@users.noreply.github.com>
2024-02-11 20:32:21 -0800

    fixed errors, adjusted logo parameters

--- index.js
+++ index.js
@@ -34,12 +34,12 @@ const generateSVG = (answers) => {
     const shapes = {
         circle: `<circle cx="150" cy="100" r="80" fill="${answers.shapeColor}" />`,
         square: `<rect x="35" y="10" width="230" height="180" fill="${answers.shapeColor}" />`,
-        triange:`<polygon points="150,10 250,190 50,190" fill="${answers.shapeColor}" />`,
+        triangle:`<polygon points="150,10 250,190 50,190" fill="${answers.shapeColor}" />`,
     }
     return `
     <svg width="300" height="200">
     ${shapes[answers.shape]}
-    <text x="50%" y="50%" text-anchor="middle" fill="${answers.textColor}" font-size="40" dy=".3em">${answers.text}</text>
+    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${answers.textColor}" font-size="64">${answers.text}</text>
     </svg>
     `;
 };
